761ab5352f7ff20afa3f81fa632b7d206b74e559
Victor <victormello.vm.oficial@gmail.com>
2024-03-18 23:25:11 -0300

    botão copiar

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 function encrypt() {
-    var input = document.getElementById("input").value.toLowerCase();
-    if (!/^[a-z]+$/.test(input)) {
-        alert("Por favor, insira apenas letras minúsculas do alfabeto.");
+    var input = document.getElementById("input").value;
+    
+    // Verifica se há letras maiúsculas no texto de entrada
+    if (/[A-Z]/.test(input)) {
+        alert("Por favor, insira apenas letras minúsculas e sem acento.");
         return;
     }
+    
     var output = input.replace(/e/g, "enter")
                       .replace(/i/g, "imes")
                       .replace(/a/g, "ai")
@@ -13,11 +16,14 @@ function encrypt() {
 }
 
 function decrypt() {
-    var input = document.getElementById("input").value.toLowerCase();
-    if (!/^[a-z]+$/.test(input)) {
-        alert("Por favor, insira apenas letras minúsculas do alfabeto.");
+    var input = document.getElementById("input").value;
+    
+    // Verifica se há letras maiúsculas no texto de entrada
+    if (/[A-Z]/.test(input)) {
+        alert("Por favor, insira apenas letras minúsculas e sem acento.");
         return;
     }
+    
     var output = input.replace(/enter/g, "e")
                       .replace(/imes/g, "i")
                       .replace(/ai/g, "a")
@@ -25,3 +31,28 @@ function decrypt() {
                       .replace(/ufat/g, "u");
     document.getElementById("input").value = output;
 }
+
+function copyToClipboard() {
+    var outputText = document.getElementById("output").innerText;
+    navigator.clipboard.writeText(outputText)
+        .then(() => {
+            alert("Texto copiado para a área de transferência.");
+        })
+        .catch(err => {
+            console.error('Erro ao copiar texto: ', err);
+        });
+}
+
+// Função para verificar se há texto na caixa de resultado e exibir/ocultar o botão de cópia conforme necessário
+function checkOutputText() {
+    var outputText = document.getElementById("output").innerText.trim();
+    var copyButton = document.getElementById("copyButton");
+    if (outputText.length > 0) {
+        copyButton.style.display = "inline-block"; // Mostra o botão
+    } else {
+        copyButton.style.display = "none"; // Oculta o botão
+    }
+}
+
+// Chama a função de verificação quando houver uma mudança na caixa de resultado
+document.getElementById("output").addEventListener("input", checkOutputText);
