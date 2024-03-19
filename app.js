ab5b29d6673ec282da16587f4dcfcbf4adb2b2d8
Victor <victormello.vm.oficial@gmail.com>
2024-03-18 21:51:08 -0300

    caixa de resposta única

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 function encrypt() {
     var input = document.getElementById("input").value.toLowerCase();
     if (!/^[a-z]+$/.test(input)) {
-        alert("Por favor, insira apenas letras minúsculas.");
+        alert("Por favor, insira apenas letras minúsculas do alfabeto.");
         return;
     }
-    var output = document.getElementById("output");
-    output.value = input.replace(/e/g, "enter")
+    var output = input.replace(/e/g, "enter")
                       .replace(/i/g, "imes")
                       .replace(/a/g, "ai")
                       .replace(/o/g, "ober")
                       .replace(/u/g, "ufat");
+    document.getElementById("input").value = output;
 }
 
 function decrypt() {
     var input = document.getElementById("input").value.toLowerCase();
     if (!/^[a-z]+$/.test(input)) {
-        alert("Por favor, insira apenas letras minúsculas.");
+        alert("Por favor, insira apenas letras minúsculas do alfabeto.");
         return;
     }
-    var output = document.getElementById("output");
-    output.value = input.replace(/enter/g, "e")
+    var output = input.replace(/enter/g, "e")
                       .replace(/imes/g, "i")
                       .replace(/ai/g, "a")
                       .replace(/ober/g, "o")
                       .replace(/ufat/g, "u");
+    document.getElementById("input").value = output;
 }
